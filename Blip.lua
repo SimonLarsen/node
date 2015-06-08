9696317a8804dd3ad2482288b1e90601492960ab
Simon Larsen <simonhffh@gmail.com>
2015-06-07 22:29:09 -0400

    Increased blip lifetime, time showed by blink speed

--- Blip.lua
+++ Blip.lua
@@ -3,7 +3,7 @@ local Blip = class("Blip", Entity)
 
 Blip.static.SPEED = 400
 Blip.static.RANGE = 75
-Blip.static.LIFETIME = 2
+Blip.static.LIFETIME = 3
 
 function Blip:initialize(x, y, xspeed, yspeed)
 	Entity.initialize(self, x, y)
@@ -12,9 +12,9 @@ function Blip:initialize(x, y, xspeed, yspeed)
 	self.xspeed = xspeed or 0
 	self.yspeed = yspeed or 0
 	self.follow = false
-	self.time = Blip.static.LIFETIME
+	self.time = 0
 
-	self.animation = Animation(Resources.getImage("blip.png"), 8, 8, 0.1)
+	self.animation = Animation(Resources.getImage("blip.png"), 8, 8, 0.07)
 	self.collider = BoxCollider(2, 2)
 end
 
@@ -23,24 +23,29 @@ function Blip:enter()
 end
 
 function Blip:update(dt)
-	self.time = self.time - dt
-	if self.time <= 0 then
-		self:kill()
-	end
+	self.time = self.time + dt
 
+	self.animation:update(dt * (self.time / Blip.static.LIFETIME))
+	
 	if self.xspeed^2 + self.yspeed^2 < 5 then
 		self.follow = true
 	end
 
+	local following = false
 	if self.follow then
 		local dx = self.player.x - self.x
 		local dy = self.player.y - self.y
 		local dist = vector.length(dx, dy)
 
 		if dist < Blip.static.RANGE then
+			following = true
 			self.xspeed = dx / dist * Blip.static.SPEED
 			self.yspeed = dy / dist * Blip.static.SPEED
 		end
+	end
+
+	if self.time >= Blip.static.LIFETIME and not following then
+		self:kill()
 	end
 
 	self.xspeed = math.movetowards(self.xspeed, 0, 500*dt)
@@ -48,8 +53,6 @@ function Blip:update(dt)
 
 	self.x = self.x + self.xspeed * dt
 	self.y = self.y + self.yspeed * dt
-
-	self.animation:update(dt)
 end
 
 function Blip:draw()
