1da47c67e0d6a06fb7526ce463d12e723af2291c
Simon Larsen <simonhffh@gmail.com>
2015-06-09 19:24:37 -0400

    Made blips follow player from all distances

--- Blip.lua
+++ Blip.lua
@@ -3,19 +3,19 @@ local Blip = class("Blip", Entity)
 
 Blip.static.SPEED = 400
 Blip.static.RANGE = 75
-Blip.static.LIFETIME = 3
+Blip.static.LIFETIME = 4
 
 function Blip:initialize(x, y, xspeed, yspeed)
 	Entity.initialize(self, x, y)
 	self:setName("blip")
 
+	self.acceleration = 500
 	self.xspeed = xspeed or 0
 	self.yspeed = yspeed or 0
-	self.follow = false
 	self.time = 0
 
-	self.animation = Animation(Resources.getImage("blip.png"), 8, 8, 0.07)
-	self.collider = BoxCollider(2, 2)
+	self.animation = Animation(Resources.getImage("blip.png"), 9, 9, 0.07)
+	self.collider = BoxCollider(4, 4)
 end
 
 function Blip:enter()
@@ -23,40 +23,33 @@ function Blip:enter()
 end
 
 function Blip:update(dt)
+	self.animation:update(dt)
+
 	self.time = self.time + dt
-
-	self.animation:update(dt * (self.time / Blip.static.LIFETIME))
 	
-	if self.xspeed^2 + self.yspeed^2 < 5 then
-		self.follow = true
-	end
+	local dx = self.player.x - self.x
+	local dy = self.player.y - self.y
+	local dist = vector.length(dx, dy)
 
-	local following = false
-	if self.follow then
-		local dx = self.player.x - self.x
-		local dy = self.player.y - self.y
-		local dist = vector.length(dx, dy)
-
-		if dist < Blip.static.RANGE then
-			following = true
-			self.xspeed = dx / dist * Blip.static.SPEED
-			self.yspeed = dy / dist * Blip.static.SPEED
-		end
-	end
+	local txspeed = dx / dist * Blip.static.SPEED
+	local tyspeed = dy / dist * Blip.static.SPEED
+	self.xspeed = math.movetowards(self.xspeed, txspeed, self.acceleration*dt)
+	self.yspeed = math.movetowards(self.yspeed, tyspeed, self.acceleration*dt)
+	self.acceleration = self.acceleration + 200*dt
 
 	if self.time >= Blip.static.LIFETIME and not following then
 		self:kill()
 	end
-
-	self.xspeed = math.movetowards(self.xspeed, 0, 500*dt)
-	self.yspeed = math.movetowards(self.yspeed, 0, 500*dt)
 
 	self.x = self.x + self.xspeed * dt
 	self.y = self.y + self.yspeed * dt
 end
 
 function Blip:draw()
-	self.animation:draw(self.x, self.y)
+	love.graphics.setColor(0, 0, 0, 128)
+	love.graphics.circle("fill", self.x, self.y, 4.5, 8)
+	love.graphics.setColor(255, 255, 255, 255)
+	self.animation:draw(self.x, self.y-16)
 end
 
 return Blip
